fa9bdfca68d869f5b46abab1187b31138f1f47b6
JEPTUI <alicejeptui@gmail.com>
2023-07-23 03:20:35 -0700

    Updating file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,8 +32,16 @@ class FileStorage:
 
         return classes.get(class_name)
 
-    def all(self):
+    def all(self, cls=None):
         """Return dictionary of all stored objects."""
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            cls_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == cls:
+                    cls_dict[k] = v
+            return cls_dict
         return self.__objects
 
     def new(self, obj):
