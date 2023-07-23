fb0d977a029ff88b1261cadebe582c1711671819
JEPTUI <alicejeptui@gmail.com>
2023-07-23 03:18:04 -0700

    Updating file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,67 +4,96 @@ import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+    """Implements simple file storage."""
+
+    __file_path = "file.json"
     __objects = {}
 
-    def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-        if cls is None or isinstance(cls, str):
-            return self.__objects
-        cls_name = cls.__name__
-        dct = {}
-        for key in self.__objects.keys():
-            if key.split('.')[0] == cls_name:
-                dct[key] = self.__objects[key]
-        return dct
-
-    def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-
-    def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
-
-    def reload(self):
-        """Loads storage dictionary from file"""
+    @staticmethod
+    def get_constructor(class_name):
+        """Return a valid class constructor given a valid class name."""
         from models.base_model import BaseModel
         from models.user import User
-        from models.place import Place
         from models.state import State
         from models.city import City
         from models.amenity import Amenity
+        from models.place import Place
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
+        }
+
+        return classes.get(class_name)
+
+    def all(self):
+        """Return dictionary of all stored objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Add new obj to __objects with key <obj class name>.id."""
+        obj_key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[obj_key] = obj
+
+    def save(self):
+        """Serialize __objects to JSON file."""
+        objects_serialized = {}
+
+        # ensure every object in __objects can be serialized
+        for obj_key, obj in self.__objects.items():
+            try:
+                objects_serialized[obj_key] = obj.to_dict()
+            except Exception:
+                objects_serialized[obj_key] = obj.__dict__
+
+        # write objects_serialized to file_path
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+            with open(self.__file_path, "w") as f_write:
+                json.dump(objects_serialized, f_write)
+        except Exception:
+            raise Exception(f"Error: Could not write to {self.__file_path}")
+
+    def reload(self):
+        """Deserializes the JSON file to __objects."""
+        try:
+            # remove all objects currently in memory
+            self.__objects.clear()
+
+            objects_loaded = {}
+
+            # read content of file_path
+            try:
+                with open(self.__file_path, "r") as f_read:
+                    objects_loaded = json.load(f_read)
+            except Exception:
+                pass
+
+            # deserialize the objects in objects_loaded
+            for obj_key, obj_dict in objects_loaded.items():
+                class_name = obj_dict.get("__class__")
+                constructor = self.get_constructor(class_name)
+
+                if constructor is None:
+                    raise Exception(f"Error: Invalid class name: {class_name}")
+
+                self.__objects[obj_key] = constructor(**obj_dict)
+
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        ''' deletes the object obj from the attribute
-            __objects if it's inside it
-        '''
-        if obj is not None:
-            key = str(obj.__class__.__name__) + "." + str(obj.id)
-            FileStorage.__objects.pop(key, None)
-            self.save()
+        """Delete a given object from __objects, if it exists."""
+        try:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+        except (AttributeError, KeyError):
+            pass
 
     def close(self):
         """method for deserializing the JSON file to objects"""
-'        self.reload()
+        self.reload()
