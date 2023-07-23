76d1604c704dd4f16d32b6f14671a0d41b05df59
JEPTUI <alicejeptui@gmail.com>
2023-07-23 02:23:08 -0700

    update Fillestorage and DBstorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,6 +55,7 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
     def delete(self, obj=None):
         ''' deletes the object obj from the attribute
             __objects if it's inside it
@@ -63,3 +64,7 @@ class FileStorage:
             key = str(obj.__class__.__name__) + "." + str(obj.id)
             FileStorage.__objects.pop(key, None)
             self.save()
+
+    def close(self):
+        """method for deserializing the JSON file to objects"""
+'        self.reload()
